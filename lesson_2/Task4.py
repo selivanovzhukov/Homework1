ea70e4cf39900b25dbda4a240fc69181102f390f
selivanovzhukov <40232998+selivanovzhukov@users.noreply.github.com>
2020-09-05 16:19:04 +0300

    Update

--- lesson_2/Task4.py
+++ lesson_2/Task4.py
@@ -1,5 +1,5 @@
 if __name__ == "__main__":
-    y = abs(int(input('Please enter your year: ')))
+    y = abs(int(float(input('Please enter your year: '))))
     print('Your year is:', y)
     if (y % 4 == 0) and (y % 100 != 0) or (y % 400 != 0):
         print('Yes')
