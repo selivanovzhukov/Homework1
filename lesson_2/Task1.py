0cf94c76ce2099b44cca11942863357a83e2da45
selivanovzhukov <40232998+selivanovzhukov@users.noreply.github.com>
2020-09-05 16:16:21 +0300

    Update

--- lesson_2/Task1.py
+++ lesson_2/Task1.py
@@ -1,5 +1,2 @@
-# if __name__ == "__main__":
-#     name = input('Please enter your name: ')
-#     print('Hello,', name, '\b!')
 name = input('Please enter your name: ')
 print(f'Hello, ' + name + '!')
--- lesson_2/Task2.py
+++ lesson_2/Task2.py
@@ -1,4 +1,4 @@
 if __name__ == "__main__":
     num1 = int(input('Please enter an integer: '))
-    print('The next number for the number', num1, 'is', num1 + 1, '\b.')
-    print('The previous number for the number', num1, 'is', num1 - 1, '\b.')
+    print(f'The next number for the number {num1} is {num1 + 1}')
+    print(f'The previous number for the number {num1} is {num1 - 1}')
