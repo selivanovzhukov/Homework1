b0ea14da251ff460d480ff9b91cf71615eb569d5
selivanovzhukov <40232998+selivanovzhukov@users.noreply.github.com>
2020-09-05 15:29:50 +0300

    Update

--- lesson_2/Task1.py
+++ lesson_2/Task1.py
@@ -1,3 +1,5 @@
-if __name__ == "__main__":
-    name = input('Please enter your name: ')
-    print('Hello,', name, '\b!')
+# if __name__ == "__main__":
+#     name = input('Please enter your name: ')
+#     print('Hello,', name, '\b!')
+name = input('Please enter your name: ')
+print(f'Hello, ' + name + '!')
