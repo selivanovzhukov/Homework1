3f29316ce19e4aa619aa73475687c9175b89665e
selivanovzhukov <40232998+selivanovzhukov@users.noreply.github.com>
2020-09-24 17:23:08 +0300

    Homework9 TBD

--- lesson_8/les8_task.py
+++ lesson_8/les8_task.py
@@ -5,17 +5,18 @@ import json
 import datetime
 
 with open('symbols.json', 'r') as f:
-    f = json.load(f)
+    symbols = json.load(f)
 
 currency_from = str(input('Enter value: '))
-for symbols in f:
+for currency_from in symbols['symbols']:
     if currency_from not in symbols:
         print('curr_from incorrect, using default value')
         currency_from = 'USD'
 currency_to = str(input('Enter value:'))
-if currency_to not in f:
-    print('curr_to incorrect, using default value')
-    currency_to = 'UAH'
+for currency_to in symbols['symbols']:
+    if currency_to not in symbols:
+        print('curr_to incorrect, using default value')
+        currency_to = 'UAH'
 amount = float(input('enter:'))
 if amount <= 0:
     amount = 100.00
