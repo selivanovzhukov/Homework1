744c935ef29b789562b523acccffc7fd56fa1005
selivanovzhukov <40232998+selivanovzhukov@users.noreply.github.com>
2020-09-24 15:15:17 +0300

    Homework7

--- lesson_7/les7_task2.py
+++ lesson_7/les7_task2.py
@@ -8,17 +8,16 @@ def temp_calc(temp_value, temp_unit):
         c = temp_value + 273.15
         f = int(float((temp_value + 459.67) / 1.8))
         print(f'The temperature in Kelvins is {k}, in Celsius is {c}, in Fahrenheits is {f}.')
-    elif temp_unit == 'C':
+    if temp_unit == 'C':
         c = temp_value
         k = temp_value - 273.15
         f = int(float(temp_value - 32)) / 1.8
         print(f'The temperature in Celsius is {c}, in Kelvins is {k}, in Fahrenheits is {f}.')
-    elif temp_unit == 'F':
+    if temp_unit == 'F':
         f = temp_value
         k = int(float(temp_value + 459.67) / 1.8)
         c = int((temp_value - 32) / 1.8)
         print(f'The temperature in Fahrenheits is {f}, in Kelvins is {k}, in Celsius is {c}.')
-    else:
-        print('')
+
 
 temp_calc(temp_value, temp_unit)
